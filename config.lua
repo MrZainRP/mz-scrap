0d6107bbe9430f832814a1b5394a7e9998450360
Mister Zain <95512163+MrZainRP@users.noreply.github.com>
2022-08-27 00:37:28 +1000

    v1.1 revamp

--- config.lua
+++ config.lua
@@ -1,5 +1,11 @@
 Config = Config or {}
 
+Config.NotifyType = 'okok' -- notification type: 'qb' for qb-core standard notifications, 'okok' for okokNotify notifications
+
+Config.mzskills = 'no' -- change to 'no' if you wish to use this resource without using mz-skills
+
+--DEFAULT CRAFT LOCATION: 1176.08, 2635.15, 37.75
+
 Config.SellLocation = {
     [1] = {
             coords = vector3(-54.9, 6392.3, 31.62),
@@ -13,12 +19,181 @@ Config.SellLocation = {
         },
     }
 
-Config.BankMoney = false -- Set to true if you want the money to go into the players bank
-Config.UseTimes = false -- Set to false if you want the pawnshop open 24/7
-
-Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
-
-Config.NotifyType = 'okok' -- notification type: 'qb' for qb-core standard notifications, 'okok' for okokNotify notifications
+------------------
+--HAND SCRAPPING--
+------------------
+--if using mz-skills, the following will apply for hand scrapping:
+Config.handXPlow = 1 -- Lowest possible amount of XP for hand scrapping.
+Config.handXPhigh = 3 -- Highest possible amount of XP for hand scrapping.
+Config.handXPloss = 2 -- Amount of XP lost if skill check failed
+-----------------------------------------------------------------------------------
+Config.handsearchlow = 3 -- Lowest time (in seconds) to search a wreck by hand
+Config.handsearchhigh = 6 -- Lowest time (in seconds) to search a wreck by hand
+Config.Handskillcheck = true -- change to 'false' if you do not want to skill check when searching by hand
+--if the above is set to 'true' the following parameters apply:
+Config.handscraplow = 1 -- Lowest number of parses to scrap a wreck by hand.
+Config.handscraphigh = 2 -- Highest number of parses to scrap a wreck by hand.
+Config.handsearchtime = 13 -- time given to player regarding skill check
+-----------------------------------------------------------------------------------
+-------------------------
+--SCREWDRIVER SCRAPPING--
+-------------------------
+--if using mz-skills, the following will apply for screwdriver scrapping:
+Config.screwdriverXP = 400 -- Minimum amount of "Scraping" XP to access screwdriver salvaging
+Config.screwdriverXPlow = 1 -- Lowest possible amount of XP for screwdriver scrapping.
+Config.screwdriverXPmid = 2 -- Mid possible amount of XP for screwdriver scrapping.
+Config.screwdriverXPhigh = 4 -- Highest possible amount of XP for screwdriver scrapping.
+Config.screwdriverXPloss = 3 -- Amount of XP lost for failing skillcheck
+-----------------------------------------------------------------------------------
+Config.screwdriversuccess = 2 -- Chance (in %) for the screwdriver to break from wear and tear (Set to 0 if you want to disable).
+Config.screwsearchlow = 3 -- Lowest time (in seconds) to search a wreck with a screwdriver.
+Config.screwsearchhigh = 8 -- Lowest time (in seconds) to search a wreck with a screwdriver.
+Config.Screwdriverskillcheck = false -- change to 'false' if you do not want to skill check when searching by screwdriver.
+--if the above is set to 'true' the following parameters apply:
+Config.screwscraplow = 2 -- Lowest number of parses to scrap a wreck with a screwdriver.
+Config.screwcraphigh = 4 -- Highest number of parses to scrap a wreck with a screwdriver.
+Config.screwsearchtime = 12 -- time given to player regarding skill check
+Config.screwdriverfail = 10 -- Chance (in %) for the screwdriver to break if skillcheck fails (Set to 0 if you want to disable).
+-----------------------------------------------------------------------------------
+-----------------------
+--BLOWTORCH SCRAPPING--
+-----------------------
+--if using mz-skills, the following will apply for blowtorch scrapping:
+Config.blowtorchXP = 1600 -- Minimum amount of "Scraping" XP to access blowtorch salvaging
+Config.blowtorchXPlow = 2 -- Lowest possible amount of XP for blowtorch scrapping.
+Config.blowtorchXPmid = 3 -- Mid possible amount of XP for blowtorch scrapping.
+Config.blowtorchXPhigh = 5 -- Highest possible amount of XP for blowtorch scrapping.
+Config.blowtorchXPloss = 4 -- Amount of XP lost for failing skill check with blowtorch.
+-----------------------------------------------------------------------------------
+Config.blowtorchsuccess = 2 -- Chance (in %) for the blowtorch to burn out from wear and tear (Set to 0 if you want to disable).
+Config.blowsearchlow = 3 -- Lowest time (in seconds) to search a wreck with a screwdriver.
+Config.blowsearchhigh = 8 -- Lowest time (in seconds) to search a wreck with a screwdriver.
+Config.Blowtorchskillcheck = true -- change to 'false' if you do not want to skill check when searching by screwdriver.
+--if the above is set to 'true' the following parameters apply:
+Config.blowscraplow = 3 -- Lowest number of parses to scrap a wreck with a screwdriver.
+Config.blowscraphigh = 6 -- Highest number of parses to scrap a wreck with a screwdriver.
+Config.blowsearchtime = 10 -- time given to player regarding skill check
+Config.blowtorchfail = 15 -- Chance (in %) for the screwdriver to break if skillcheck fails (Set to 0 if you want to disable).
+-----------------------------------------------------------------------------------
+------------
+--CRAFTING--
+------------
+-----------------------
+--BREAKING DOWN TIRES--
+-----------------------
+Config.tirelow = 4 -- Lowest number of skillcheck parses to break down tires
+Config.tirehigh = 6 -- Highest number of skillcheck parses to break down tires
+Config.tiretimelow = 10 -- Lowest time (in seconds) to strip down tires.
+Config.tiretimehigh = 15 -- Highest time (in seconds) to strip down tires.
+Config.tiresneeded = 5 -- Number of tires needed to perform breakdown
+Config.Rubberoutputlow = 15 -- Lowest amount of rubber from tire breakdown
+Config.Rubberoutputhigh = 20 -- Highest amount of rubber from tire breakdown
+--If you are using mz-skills, the following parameters apply:
+Config.tireXPlow = 2 -- Lowest amount of "Scraping" XP obtained for breaking down tires.
+Config.tireXPhigh = 4 -- Highest amount of "Scraping" XP obtained for breaking down tires.
+Config.tireXPloss = 2 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
+-----------------------------------------------------------------------------------
+------------------
+--CLEANING NAILS--
+------------------
+Config.naillow = 4 -- Lowest number of skillcheck parses to clean nails.
+Config.nailhigh = 6 -- Highest number of skillcheck parses to clean nails.
+Config.nailtimelow = 10 -- Lowest time (in seconds) to clean nails.
+Config.nailtimehigh = 15 -- Highest time (in seconds) to clean nails.
+Config.nailsneeded = 5 -- Number of nails needed to perform clean
+Config.metaloutputlow = 15 -- Lowest amount of metalscrap from nails.
+Config.metaloutputhigh = 20 -- Highest amount of metalscrap from nails.
+--If you are using mz-skills, the following parameters apply:
+Config.nailXPlow = 2 -- Lowest amount of "Scraping" XP obtained for cleaning nails.
+Config.nailXPhigh = 4 -- Highest amount of "Scraping" XP obtained for cleaning nails.
+Config.nailXPloss = 2 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
+-----------------------------------------------------------------------------------
+---------------------
+--BREAK APART RADIO--
+---------------------
+Config.radiolow = 7 -- Lowest number of skillcheck parses to break apart radio.
+Config.radiohigh = 9 -- Highest number of skillcheck parses to break apart radio.
+Config.radiotimelow = 10 -- Lowest time (in seconds) to break apart radio.
+Config.radiotimehigh = 15 -- Highest time (in seconds) to break apart radio.
+Config.radiosneeded = 3 -- Number of radio needed to strip components.
+Config.elecoutputlow = 7 -- Lowest amount of electric scrap from radio components. 
+Config.elecoutputhigh = 11 -- Highest amount of electric scrap from radio components. 
+--If you are using mz-skills, the following parameters apply:
+Config.radioXPlow = 2 -- Lowest amount of "Scraping" XP obtained to break apart radio.
+Config.radioXPhigh = 4 -- Highest amount of "Scraping" XP obtained to break apart radio.
+Config.radioXPloss = 2 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
+-----------------------------------------------------------------------------------
+-------------
+--CAR JACKS--
+-------------
+Config.jackslow = 7 -- Lowest number of skillcheck parses to break apart jacks.
+Config.jackshigh = 9 -- Highest number of skillcheck parses to break apart jacks.
+Config.jackstimelow = 10 -- Lowest time (in seconds) to break apart jacks.
+Config.jackstimehigh = 15 -- Highest time (in seconds) to break apart jacks.
+Config.jacksneeded = 3 -- Number of jacks needed to break apart.
+Config.jacksmetaloutputlow = 6 -- Lowest amount of metal scrap from broken up jacks. 
+Config.jacksmetaloutputhigh = 9 -- Highest amount of metal scrap from broken up jacks. 
+Config.jackssteeloutputlow = 3 -- Lowest amount of steel from broken up jacks. 
+Config.jackssteeloutputlow = 6 -- Highest amount of steel from broken up jacks. 
+--If you are using mz-skills, the following parameters apply:
+Config.jacksXPlow = 2 -- Lowest amount of "Scraping" XP obtained to break apart jacks.
+Config.jacksXPhigh = 4 -- Highest amount of "Scraping" XP obtained to break apart jacks.
+Config.jacksXPloss = 2 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
+-----------------------------------------------------------------------------------
+-------------
+--CAR DOORS--
+-------------
+Config.doorslow = 6 -- Lowest number of skillcheck parses to break apart car doors.
+Config.doorshigh = 8 -- Highest number of skillcheck parses to break apart car doors.
+Config.doorstimelow = 10 -- Lowest time (in seconds) to break apart car doors.
+Config.doorstimehigh = 15 -- Highest time (in seconds) to break apart car doors.
+Config.doorsneeded = 3 -- Number of doors needed to break apart.
+Config.doorglasslow = 5 -- Lowest amount of glass from broken up doors.
+Config.doorglasshigh = 15 -- Highest amount of glass from broken up doors.
+Config.doorironlow = 3 -- Lowest amount of iron from broken up doors.
+Config.doorironhigh = 6 -- Highest amount of iron from broken up doors.
+--If you are using mz-skills, the following parameters apply:
+Config.doorsXPlow = 2 -- Lowest amount of "Scraping" XP obtained to break apart car doors.
+Config.doorsXPhigh = 4 -- Highest amount of "Scraping" XP obtained to break apart car doors.
+Config.doorsXPloss = 2 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
+-----------------------------------------------------------------------------------
+-------------
+--CAR HOODS--
+-------------
+Config.hoodslow = 6 -- Lowest number of skillcheck parses to break apart car hood.
+Config.hoodshigh = 8 -- Highest number of skillcheck parses to break apart car hood.
+Config.hoodstimelow = 10 -- Lowest time (in seconds) to break apart car hood.
+Config.hoodstimehigh = 15 -- Highest time (in seconds) to break apart car hood.
+Config.hoodneeded = 3 -- Number of hoods needed to break apart.
+Config.hoodscraplow = 5 -- Lowest amount of metal scrap from broken up hood.
+Config.hoodscraphigh = 15 -- Highest amount of metal scrap from broken up hood.
+Config.hoodcopperlow = 3 -- Lowest amount of copper from broken up hood.
+Config.hoodcopperhigh = 6 -- Highest amount of copper from broken up hood.
+--If you are using mz-skills, the following parameters apply:
+Config.hoodsXPlow = 2 -- Lowest amount of "Scraping" XP obtained to break apart car hood.
+Config.hoodsXPhigh = 4 -- Highest amount of "Scraping" XP obtained to break apart car hood.
+Config.hoodsXPloss = 2 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
+-----------------------------------------------------------------------------------
+--------------
+--CAR ENGINE--
+--------------
+Config.enginelow = 10 -- Lowest number of skillcheck parses to break apart car engine.
+Config.enginehigh = 13 -- Highest number of skillcheck parses to break apart car engine.
+Config.enginetimelow = 20 -- Lowest time (in seconds) to break apart car engine.
+Config.enginetimehigh = 30 -- Highest time (in seconds) to break apart car engine.
+Config.engineneeded = 1 -- Number of engines needed to break apart.
+Config.enginescraplow = 7 -- Lowest amount of metal scrap from a broken up engine.
+Config.enginescraphigh = 12 -- Highest amount of metal scrap from a broken up engine.
+Config.enginecopperlow = 4 -- Lowest amount of copper from a broken up engine.
+Config.enginecopperhigh = 8 -- Highest amount of copper from a broken up engine.
+Config.engineironlow = 3 -- Lowest amount of iron from a broken up engine.
+Config.engineironhigh = 6 -- Highest amount of iron from a broken up engine.
+Config.enginesteellow = 5 -- Lowest amount of steel from a broken up engine.
+Config.enginesteelhigh = 10 -- Highest amount of steel from a broken up engine.
+--If you are using mz-skills, the following parameters apply:
+Config.engineXPlow = 5 -- Lowest amount of "Scraping" XP obtained to break apart car engine.
+Config.engineXPhigh = 8 -- Highest amount of "Scraping" XP obtained to break apart car engine.
+Config.engineXPloss = 5 -- Amount of "Scraping" XP lost for failing crafting skillcheck.
 
 Config.Objects = {
     -- Scrap objects player can interact with
